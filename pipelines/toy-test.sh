8c8ad660111c1f20d4894f5677ce7738a46dfa3c
Nathan Kolbow <nkolbow@wisc.edu>
2023-10-17 11:45:00 -0500

    Added toy example test

---
+++ pipelines/toy-test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Tests the pipeline on a toy example that runs quickly.
+
+resultfile="../results/results.csv"
+netnewick="((A:1.0,((B:1.0,C:1.0):1.0,(D:1.0)#H1:1.0::0.5):1.0):1.0,(#H1:1.0::0.5,E:1.0):1.0);"
+nproc=4
+ngt=5
+
+./run-one.sh "${netnewick}" ${resultfile} ${ngt} ${nproc}
