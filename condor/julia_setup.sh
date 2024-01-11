68df1f273fb4d5554fd63ddeedded59de0b7f0d0
NathanKolbow <nathankolbow@gmail.com>
2024-01-11 13:22:30 -0600

    small adjustments after debugging

--- condor/julia_setup.sh
+++ condor/julia_setup.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 
-# Install Julia
+# Unpack and install julia
 tar -xzf julia-1.9.3-linux-x86_64.tar.gz
 export PATH=$PWD/julia-1.9.3/bin:$PATH
 
-# Install dendropy for gtee
-pip3 install dendropy --user
-
-# Install relevant Julia packages
+# Install julia packages
 julia julia-package-install.jl
 
-# Navigate to the relevant folder
-# cd /mnt/ws/home/nkolbow/snaq2/pipelines/
+# Install dendropy
+pip3 install dendropy --user
 
 # Run stuff
 # - $1: network newick (already quoted in the submit file)
