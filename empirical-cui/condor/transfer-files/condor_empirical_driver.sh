aed0da375508cc5a38081d16f3dcde93106e1f8e
NathanKolbow <nathankolbow@gmail.com>
2024-04-30 11:39:45 -0500

    replaced test data w/ real data

--- empirical-cui/condor/transfer-files/condor_empirical_driver.sh
+++ empirical-cui/condor/transfer-files/condor_empirical_driver.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 set -e
 
+echo "Entering bash script"
+
 # Setup Julia
+echo "Unpacking Julia"
 tar -xzf julia-1.9.3-linux-x86_64.tar.gz
+echo "Unpacking project"
 tar -xzf snaq2-proj.tar.gz
 export PATH=$PWD/julia-1.9.3/bin:$PATH
 export JULIA_DEPOT_PATH=$PWD/snaq2-proj/
@@ -11,4 +15,5 @@ export JULIA_DEPOT_PATH=$PWD/snaq2-proj/
 num_hybrids=$1
 
 # Run the script
+echo "Running Julia script from bash"
 julia --project=snaq2-proj/ -p16 -t16 ./script.jl ${num_hybrids}
