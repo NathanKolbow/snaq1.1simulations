40339932ac2ba23179a4434874ed2a8659d47081
NathanKolbow <nathankolbow@gmail.com>
2024-04-30 11:39:56 -0500

    replaced test data w/ real data

--- empirical-cui/condor/transfer-files/script.jl
+++ empirical-cui/condor/transfer-files/script.jl
@@ -1,3 +1,4 @@
+@info "Entering Julia script"
 using Distributed
 
 # Verify number of processors
@@ -17,22 +18,27 @@ end
 
 
 # Load PhyloNetworks
+@info "Loading PhyloNetworks"
 @everywhere using PhyloNetworks
-pqr = 1     # fully weighted
+pqr = 1.     # fully weighted
 pqt = 0.7   # use a portion of quartets
 nhybrids = parse(Int64, ARGS[1])
 
 # Load data
-# t = readTopology("cui_etal_data.msbum.QMC.tre")   # real data
-# d = readTableCF("cui_etal_data.msbum.CFs.csv")    # real data
-t = readTopology("test_data.tre")
-d = readTableCF("test_data.CFs.csv")
+@info "Reading starting tree"
+t = readTopology("cui_etal_data.msbum.QMC.tre")   # real data
 
+@info "Reading CF table"
+d = readTableCF("cui_etal_data.msbum.CFs.csv")    # real data
+
+# Run SNaQ
+@info "Running SNaQ"
 filename = "/mnt/ws/home/nkolbow/repos/snaq2/empirical-cui/condor/snaq_outputs/cui_net_$(nhybrids)hyb"
 time_taken = @elapsed net = snaq!(t, d, hmax=nhybrids, probQR=pqr, propQuartets=pqt, seed=42, filename=filename)
 
 
-# WRITE CODE TO SAVE RESULTS, INCLUDING RUNTIME
+# Save results
+@info "Saving runtime"
 open("$(filename).runtime", "w+") do f
     write(f, "$(time_taken)\n")
 end
