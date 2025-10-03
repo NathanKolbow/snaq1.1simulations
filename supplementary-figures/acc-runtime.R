9ced7900c2a50a8864826914cb01ed78a1166a95
NathanKolbow <nathankolbow@gmail.com>
2025-10-03 13:56:01 -0500

    added new supplementary figure

--- supplementary-figures/acc-runtime.R
+++ supplementary-figures/acc-runtime.R
@@ -3,6 +3,7 @@ library(dplyr)    # alternatively, this also loads %>%
 library(ggplot2)
 library(cowplot)
 library(patchwork)
+library(ggh4x)
 setwd("/mnt/dv/wid/projects4/SolisLemus-snaq2/data/output")
 
 
@@ -86,4 +87,57 @@ dev.off()
 pdf("../../supplementary-figures/accuracy-n20r3.pdf", width=7.7, height=4.18)
 a203 <- plot_accuracy()
 a203
+dev.off()
+
+
+
+
+plot_accuracy_by_ngt <- function() {
+    data %>%
+        group_by(numgt, whichSNaQ, propQuartets, probQR) %>%
+        summarise(
+            mean_hwcd = mean(netRF),
+            se_hwcd = sd(netRF),
+            ymin = quantile(netRF, 0.025),
+            ymax = quantile(netRF, 0.975)
+        ) %>%
+        mutate(
+            whichSNaQ = if_else(whichSNaQ == 1, "v1.0", "v1.1"),
+            propQuartets = paste0("propQuartets = ", propQuartets),
+            numgt = if_else(numgt == "300 gt", 300, if_else(numgt == "1000 gt", 1000, 3000))
+        ) %>%
+        ggplot(aes(x = numgt, y = mean_hwcd, shape = factor(whichSNaQ), color = factor(whichSNaQ))) +
+        geom_point(size=3, position = position_dodge(width=300)) +
+        geom_line(aes(group = interaction(whichSNaQ, probQR, propQuartets)), linetype = "dashed", alpha = 0.75) +
+        geom_errorbar(aes(ymin = ymin, ymax = ymax), position = position_dodge(width=300), width=300) +
+        facet_grid(probQR ~ propQuartets) +
+        scale_shape_manual(values = c(1, 4)) +
+        labs(
+            x = "Number of gene trees",
+            y = "Average Hardwired Cluster Distance",
+            color = "SNaQ Version",
+            shape = "SNaQ Version"
+        ) +
+        scale_x_continuous(
+            breaks = c(300, 1000, 3000),
+            limits = c(0, 3500)
+        ) +
+        expand_limits(x=0, y=0) +
+        theme(panel.grid.minor = element_blank())
+}
+
+data <- read_data("n10r1.csv")
+p1 <- plot_accuracy_by_ngt() + ggtitle("A: n10r1") + labs(x="")
+
+data <- read_data("n10r3.csv")
+p2 <- plot_accuracy_by_ngt() + ggtitle("B: n10r3") + labs(y="") + labs(x="")
+
+data <- read_data("n20r1.csv")
+p3 <- plot_accuracy_by_ngt() + ggtitle("C: n20r1")
+
+data <- read_data("n20r3.csv")
+p4 <- plot_accuracy_by_ngt() + ggtitle("D: n20r3") + labs(y="")
+
+pdf("../../supplementary-figures/accuracy-by-ngt-all.pdf", width=15, height=7.5)
+(p1 + p2) / (p3 + p4) + plot_layout(axis_titles = "collect", guides = "collect")
 dev.off()
